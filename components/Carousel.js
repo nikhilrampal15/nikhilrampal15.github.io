983eaba94af67948e27e419cdbd04610cf9a2c9b
nikhilrampal15 <nikhilrampal15@gmail.com>
2016-08-21 14:49:55 -0700

    fixed image url

--- components/Carousel.js
+++ components/Carousel.js
@@ -8,18 +8,18 @@ const CarouselInstance = () => (
             <Col md={8} mdOffset ={2}>
                 <Carousel indicators ={false}>
                     <Carousel.Item>
-                        <img width={900} height={500} src="./components/images/JobChaser.png"/>
+                        <img width={900} height={500} src="./components/Images/JobChaser.png"/>
                         <Carousel.Caption id="JobChaser">
                         </Carousel.Caption>
                     </Carousel.Item>
 
                     <Carousel.Item>
-                        <img width={900} height={500}  src="./components/images/LetsCook.png"/>
+                        <img width={900} height={500}  src="./components/Images/LetsCook.png"/>
                         <Carousel.Caption id="LetsCook">
                         </Carousel.Caption>
                     </Carousel.Item>
                     <Carousel.Item>
-                        <img width={900} height={500}  src="./components/images/VirtualRealtor.png"/>
+                        <img width={900} height={500}  src="./components/Images/VirtualRealtor.png"/>
                         <Carousel.Caption id="VirtualRealtor">
                         </Carousel.Caption>
                     </Carousel.Item>
